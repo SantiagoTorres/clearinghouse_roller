40a8184424b3cbd748cee0b31e268c6cb8f16eb0
Santiago Torres <torresariass@gmail.com>
2014-08-09 11:35:37 -0400

    Added executable flags and sourceme script

---
+++ sourceme.sh
@@ -0,0 +1,7 @@
+
+export PYTHONPATH=$PYTHONPATH:$PWD/tmp/:$PWD/tmp/seattle:$PWD/tmp/seattlegeni/
+export DJANGO_SETTINGS_MODULE=seattlegeni.website.settings
+
+echo $PYTHONPATH
+echo 
+echo $DJANGO_SETTINGS_MODULE
